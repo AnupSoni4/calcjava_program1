8304dee97af164ff96946bcdc206a25dfdf605b0
AnupSoni4 <anupsoni319@gmail.com>
2026-08-19 00:42:01 +0530

    PrintvsPrintln

---
+++ PrintvsPrintln.java
@@ -0,0 +1,8 @@
+public class PrintvsPrintln{
+    public static void main(String[] args) {
+        System.out.print("THis is");
+        System.out.println("it is on the same line and content after this on the next line");
+        System.out.print("now on the next line");
+        System.out.println("on the same line after this on the next line");
+    }
+}

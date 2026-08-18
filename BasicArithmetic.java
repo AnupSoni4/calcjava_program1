60eeb580df83733dd146be3847a79c29f79b29fd
AnupSoni4 <anupsoni319@gmail.com>
2026-08-19 00:24:55 +0530

    BasicArithmetic

---
+++ BasicArithmetic.java
@@ -0,0 +1,9 @@
+public class BasicArithmetic {
+    public static void main(String[] args){
+        System.out.println("sum is : " + (5+3) );
+        System.out.println("sum is : " + (3*4) );
+        
+
+    }
+    
+}

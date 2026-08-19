da54237d4aa30f1e40b354765df536aaa7ec5b6c
AnupSoni4 <anupsoni319@gmail.com>
2026-08-19 20:09:15 +0530

    MultiplrVariableInOneLine

--- MultipleVariableInOneLine.java
+++ MultipleVariableInOneLine.java
@@ -1,6 +1,8 @@
 public class MultipleVariableInOneLine {
     public static void main(String[] args){
-        
+        int x = 10 , y = 10;
+        System.out.println("x = " + x + ", y = " + y );
+
     }
     
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+ int x = 10 , y = 10;
+        System.out.println("x = " + x + ", y = " + y );
